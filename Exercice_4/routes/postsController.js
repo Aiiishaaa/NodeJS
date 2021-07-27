df7c8714b5b584e7ab1607f117bed9979f6fa374
Aiiishaaa <aicha.hamida06@yahoo.fr>
2021-07-27 14:21:40 +0200

    api-node

---
+++ Exercice_4/routes/postsController.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router();
+const ObjectID = require('mongoose').Types.ObjectId;
+
+const { PostsModel } = require('../models/postsModel');
+
+router.get('/', (req, res) => {
+    PostsModel.find((err, docs) => {
+        if (!err) res.send(docs);
+        else console.log("Error to get data : " + err);
+    })
+});
+
+router.post('/', (req, res) => {
+    const newRecord = new PostsModel({
+        author: req.body.author,
+        message: req.body.message
+    });
+
+    newRecord.save((err, docs) => {
+        if (!err) res.send(docs);
+        else console.log('Error creating new data : ' + err);
+    })
+});
+
+module.exports = router;
